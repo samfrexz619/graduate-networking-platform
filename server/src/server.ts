ddd05163c59dd15f292cd7af15a3f890916a9831
samfrexz619 <samfrexz69@gmail.com>
2026-08-28 12:57:39 +0100

    feat(server): connect to MongoDB before starting API

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,23 @@
+import dotenv from "dotenv";
 import app from "./app.js";
+import { connectDB } from "./config/db.js";
+
+
+dotenv.config();
 
 
 
 const PORT = process.env.PORT || 5001;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+const startServer = async () => {
+  await connectDB();
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
+
+startServer().catch((err) => {
+  console.error("Application startup failed", err);
+  process.exit(1);
 });
