fdaa33d0b99ff422a8ee17683d5dfa8ebada3fe9
samfrexz619 <samfrexz69@gmail.com>
2026-08-30 11:31:05 +0100

    add middleware for protected routes

---
+++ server/src/middleware/auth.middleware.ts
@@ -0,0 +1,35 @@
+import { type Response, type NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import type { AuthRequest } from "../types/auth.types.js";
+
+
+
+export const protect = (req: AuthRequest, res: Response, next: NextFunction): void => {
+
+  try {
+    const token = req.cookies.token;
+
+    if (!token) {
+      res.status(401).json({
+        success: false,
+        message: "Not authenticated"
+      });
+      return;
+    };
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as {
+      id: string;
+      role: string;
+    };
+
+    req.userId = decoded.id;
+    req.role = decoded.role;
+
+    next();
+  } catch (error) {
+    res.status(401).json({
+      success: false,
+      message: "Invalid token"
+    })
+  };
+};
---
+++ server/src/types/auth.types.ts
@@ -0,0 +1,6 @@
+import { type Request } from "express";
+
+export interface AuthRequest extends Request {
+  userId?: string;
+  role?: string;
+}
