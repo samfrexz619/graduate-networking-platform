644baff4e42e545c68991a85ec90823f29a84133
samfrexz619 <samfrexz69@gmail.com>
2026-08-30 08:27:58 +0100

    feat: add login implementation

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,5 +1,6 @@
 import { type Request, type Response } from "express";
 import { User } from "../models/User.js";
+import { generateToken } from "../utils/generateToken.js";
 
 
 
@@ -60,4 +61,61 @@ export const register = async (req: Request, res: Response): Promise<void> => {
     });
   }
 };
+
+export const login = async (req: Request, res: Response): Promise<void> => {
+
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      res.status(400).json({
+        success: false,
+        message: "Email and password are required!"
+      });
+
+      return;
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      res.status(401).json({
+        success: false,
+        message: "Invalid credentials"
+      });
+      return;
+    }
+
+    const isPasswordValid = await user.comparePassword(password);
+
+    if (!isPasswordValid) {
+      res.status(401).json({
+        success: false,
+        message: "Invalid credentials"
+      });
+      return;
+    }
+
+    const token = generateToken(user._id.toString(), user.role);
+
+    res.status(200).json({
+      success: true,
+      message: "Login successful",
+      token,
+      data: {
+        id: user._id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        role: user.role
+      }
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error"
+    });
+  }
+}
 
